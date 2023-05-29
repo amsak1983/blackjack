e11420b369658f152b378d960ca84d2c145d1c46
amsak1983 <amsak@yandex.by>
2023-05-29 19:00:55 +0300

    first commit

---
+++ card.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Card
+  def initialize(lear, face)
+    @lear = lear
+    @face = face
+  end
+
+  def value
+    return @face if @face.is_a?(Integer)
+
+    @face.eql?('A') ? 11 : 10
+  end
+
+  def display
+    "#{@face}#{@lear}"
+  end
+end
---
+++ main.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'card'
+require_relative 'deck'
+require_relative 'hand'
+require_relative 'game'
+
+Game.new
